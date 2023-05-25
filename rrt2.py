96f0881a9445910fae37fb88da66ce26141cbccb
pioliX000 <65280877+pioliX000@users.noreply.github.com>
2023-05-25 10:57:24 +0200

    Update rrt2.py
    Damn this stuff trash

--- rrt2.py
+++ rrt2.py
@@ -7,7 +7,7 @@ import requests
 
 def rrt(x):
 	output = requests.get(x, headers={"User-Agent":"curl/7.44.0"}).text.lower()
-	if "never gonna give you up" in output or "dQw4w9WgXcQ" in output:
+	if "never gonna give you up" in output or "dQw4w9WgXcQ".lower() in output:
 		y = "RUN BRUV"
 	else:
 		y ="u good bruv go ahead"
@@ -15,22 +15,21 @@ def rrt(x):
 
 def selenium(x):
 	options = Options()
-	options.headless = True
+	options.add_argument('-headless')
 	driver = webdriver.Firefox(options=options)
-	html_source = driver.execute_script("return document.body.innerHTML;")
+	html_source = driver.execute_script("return document.body.innerHTML;").lower()
 	driver.get(x)
-	time.sleep(5)
 	title = driver.title
 	print(html_source)
-	if title == "Rick Astley - Never Gonna Give You Up (Official Music Video) - YouTube":
+	if "rick" in title.lower() :
 		driver.close()
 		print("check2: RUN BRUV")
 	else:
 		try:
 			elem = driver.find_element_by_class_name("mwButton")
-			time.sleep(10)
+			time.sleep(3)
 			output = elem.get_attribute('outerHTML')
-			if "never gonna give you up" in output or "dQw4w9WgXcQ" in output:
+			if "never gonna give you up" in output or "dQw4w9WgXcQ".lower() in output:
 				y = "RUN BRUV"	
 			elif "never gonna give you up" in title or "rick astley" in title:
 				y = "RUN BRUV"
@@ -42,5 +41,12 @@ def selenium(x):
 			pass
 			print("check2: u good bruv... I think")
 
-rrt(sys.argv[1])
-selenium(sys.argv[1])
+try:
+	rrt(sys.argv[1])
+	try:
+		selenium(sys.argv[1])
+	except: 
+		print("check2: error!")
+
+except IndexError:
+	print("Supply a link!")
