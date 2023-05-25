63986b81e97a8f6dc09f8ba0c4f8192a31069d07
pioliX000 <65280877+pioliX000@users.noreply.github.com>
2023-05-25 11:05:52 +0200

    Update rrt2.py

--- rrt2.py
+++ rrt2.py
@@ -17,22 +17,16 @@ def selenium(x):
 	options = Options()
 	options.add_argument('-headless')
 	driver = webdriver.Firefox(options=options)
-	html_source = driver.execute_script("return document.body.innerHTML;").lower()
 	driver.get(x)
+	html_source = driver.execute_script("return document.body.innerHTML;").lower()		
 	title = driver.title
-	print(html_source)
 	if "rick" in title.lower() :
 		driver.close()
 		print("check2: RUN BRUV")
 	else:
 		try:
-			elem = driver.find_element_by_class_name("mwButton")
-			time.sleep(3)
-			output = elem.get_attribute('outerHTML')
-			if "never gonna give you up" in output or "dQw4w9WgXcQ".lower() in output:
+			if "never gonna give you up" in html_source or "dQw4w9WgXcQ".lower() in html_source or "rick astley" in html_source:
 				y = "RUN BRUV"	
-			elif "never gonna give you up" in title or "rick astley" in title:
-				y = "RUN BRUV"
 			else:
 				y = "u good bruv go ahead"
 			print("check2:", y)
